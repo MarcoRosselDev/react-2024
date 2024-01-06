dfe2b69cfb3b0d3d20c95f6acaefea8456c4358e
Marco Rossel <andresmarcorossel@gmail.com>
2024-01-06 13:03:27 -0300

    04 vite-color-project 2

--- 04-vite-color-project/src/App.jsx
+++ 04-vite-color-project/src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState('lightBlue')
+  const [color, setColor] = useState('lightblue')
 
   console.log(color);
   return (
-    <>
-      <h1>color project</h1>
-      <button>lightred</button>
-    </>
+    <main>
+      <div className='main-div'>
+        <h1>color project</h1>
+        <div className='contenedor'>
+          <button>lightred</button>
+          <button>ligthblue</button>
+          <button>orange</button>
+          <button>black</button>
+        </div>
+      </div>
+    </main>
   )
 }
 
