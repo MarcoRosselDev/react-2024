962ca3d22597813ddb28345b5fa6a8394d3d2d15
Marco Rossel <andresmarcorossel@gmail.com>
2024-01-07 14:37:09 -0300

    pass generator 01

--- 05-vite-pass-generator/src/App.jsx
+++ 05-vite-pass-generator/src/App.jsx
@@ -4,23 +4,24 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const [range, setRange] = useState(5)
+  const [pass, setPass] = useState('pijsn')
   return (
     <>
       <div className='input-div'>
-        <input type="text" />
+        <p className='p-pass'>{pass}</p>
         <button>trigger</button>
       </div>
       <div className='options'>
-        <div>
-          <p>digits</p>
-          <input type="range" defaultValue={5} min={5} max={15} onChange={(event) => setRange(event.target.value)}/>
-          <p>{range}</p>
-        </div>
         <p>numbers</p>
         <input type="checkbox" />
         <p>characters</p>
         <input type="checkbox" />
       </div>
+        <div className='range-css'>
+          <p>digits</p>
+          <input type="range" defaultValue={5} min={5} max={15} onChange={(event) => setRange(event.target.value)}/>
+          <p>{range}</p>
+        </div>
     </>
   )
 }
