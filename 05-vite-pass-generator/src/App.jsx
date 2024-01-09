d391920b130f5b14270e149dc015934cf790c5ab
Marco Rossel <andresmarcorossel@gmail.com>
2024-01-09 14:47:09 -0300

    pass generator 03

--- 05-vite-pass-generator/src/App.jsx
+++ 05-vite-pass-generator/src/App.jsx
@@ -4,14 +4,25 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const [range, setRange] = useState(5)
-  const [pass, setPass] = useState('pijSnaVZ')
+  const [pass, setPass] = useState()
 
-  let letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numbers = '1234567890';
+  const caracteres = '!@#$%^&*()_+|}{":;[].,?><-='
 
-  const generate_pass = (numb = false, charr = false) => {
-    console.log(numb, charr);
+  const generate_pass = (numb = false, charr = false, range_f = range ) => {
+    //console.log(numb, charr, range_f);
+    console.log(letters.length);
+    
+    /* let pass_str = '';
+    for (let i = 0; i < range_f.length; i++) {
+      let random = Math.floor(Math.random() * letters.length)
+      console.log(letters.length, 'random: ', random);
+      //Math.floor(Math.random() * letters.length);
+    } */
   }
 
+  generate_pass();
   return (
     <>
       <div className='input-div'>
