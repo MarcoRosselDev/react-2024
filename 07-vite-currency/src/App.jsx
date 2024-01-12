88b0d4f3bb5484e5832a4e1a277dd973bcca025b
Marco Rossel <andresmarcorossel@gmail.com>
2024-01-12 18:01:42 -0300

    vite-07 currency project | 4

--- 07-vite-currency/src/App.jsx
+++ 07-vite-currency/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css';
 import useCurrencyInfo from './hooks/useCurrencyInfo';
+import {InputBox} from './components/index.js'
 
 
 function App() {
@@ -9,18 +10,44 @@ function App() {
   const [to, setTo] = useState('inr');
   const [convertedAmount, setConvertedAmount] = useState(0);
 
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo) // no se que hace esta linea por ahora;
 
+  // boton invertir los valores de convercion
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount)
+  }
 
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
 
   return (
     <>
       <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
-    style={{backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`}}
-    >
-
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
+      style={{backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`}}>
+        <div className='w-full'>
+          <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30'>
+            <form onSubmit={(event) => {
+              event.preventDefault();
+              convert();
+            }}>
+              <div className='w-full mb-1'>
+                <InputBox
+                label="from"
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setFrom(currency)}
+                onAmountChange={(amount) => setAmount(amount)}
+                selectedCurrency={from}
+                />
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </>
   )
