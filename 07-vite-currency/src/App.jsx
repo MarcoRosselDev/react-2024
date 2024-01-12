f1a84633610fd0804c03842897eac081037fde63
Marco Rossel <andresmarcorossel@gmail.com>
2024-01-12 17:37:43 -0300

    vite-07 currency project | 3

--- 07-vite-currency/src/App.jsx
+++ 07-vite-currency/src/App.jsx
@@ -1,8 +1,16 @@
 import { useState } from 'react'
-import './App.css'
+import './App.css';
+import useCurrencyInfo from './hooks/useCurrencyInfo';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState('usd');
+  const [to, setTo] = useState('inr');
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+
+
 
   return (
     <>
