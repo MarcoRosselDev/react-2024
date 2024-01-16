1763dccab96161daff3e4743240f317e55649e30
Marco Rossel <andresmarcorossel@gmail.com>
2024-01-16 02:44:47 -0300

    vite-08 | react router | 8

--- 08-react-router/src/main.jsx
+++ 08-react-router/src/main.jsx
@@ -5,7 +5,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css';
-import Root from './routes/root';
+import Root, {loader as rootLoader} from './routes/root';
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
 
@@ -13,12 +13,15 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,  // elemento raiz
-    errorElement: <ErrorPage /> // elemento ooops! error generico
+    errorElement: <ErrorPage />, // elemento ooops! error generico
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      }
+    ]
   },
-  {
-    path: "contacts/:contactId",
-    element: <Contact />
-  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
