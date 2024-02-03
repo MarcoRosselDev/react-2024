fa8b51197257d9a64eb211d0226bdf90d91c2ca3
Marco Rossel <andresmarcorossel@gmail.com>
2024-02-02 23:11:31 -0300

    React project reminder 12 | 4

--- 12-and/src/App-challenge.jsx
+++ 12-and/src/App-challenge.jsx
@@ -1,6 +1,43 @@
+import { useState } from "react"
+
 export default function App(params) {
+
+  const [formState, setFormState] = useState({
+    name:'',
+    last_name: '',
+  });
+
+  function setFormFunction(event) {
+    
+    console.log(event.name);
+
+    setFormFunction(prev => {
+      return {
+        ...prev,
+        
+      }
+    })
+  }
+
   return (
     <>
+      <form >
+
+        <label htmlFor="name">name</label>
+        <input 
+        type="text" 
+        placeholder="name..."
+        name="name"
+        onChange={setFormFunction}
+        />
+        <label htmlFor="last_name">last name</label>
+        <input 
+        type="text" 
+        placeholder="last name..."
+        name="last_name"
+        onChange={setFormFunction}
+        />
+      </form>
       <h1>Hi</h1>
     </>
   )
