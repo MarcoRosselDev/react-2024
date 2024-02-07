47720ef89ae682db7bd3ddf9c2668b9f592e0a4d
Marco Rossel <andresmarcorossel@gmail.com>
2024-02-06 23:05:05 -0300

    React project reminder 15 | 2

--- 15-toggle-questions/src/App.jsx
+++ 15-toggle-questions/src/App.jsx
@@ -2,11 +2,36 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [mostras, setMostrar] = useState(false)
+
+  function hidden(elem) {
+    console.log(elem.target.id);
+    setMostrar(anterior => !anterior)
+  }
 
   return (
     <>
-      <p>question</p>
+      <p>preguntas frecuentes</p>
+
+      <div className='contenedor-preguntas'>
+        <div className='pregunta'>
+          <p onClick={hidden} id='tarea 1'>+ titulo</p>
+          <p>{mostras && 'contenido'}</p>
+        </div>
+        <div className='pregunta'>
+          <p onClick={hidden}>+ titulo</p>
+          <p>{mostras && 'contenido'}</p>
+        </div>
+        <div className='pregunta'>
+          <p onClick={hidden}>+ titulo</p>
+          <p>{mostras && 'contenido'}</p>
+        </div>
+        <div className='pregunta'>
+          <p onClick={hidden}>+ titulo</p>
+          <p>{mostras && 'contenido'}</p>
+        </div>
+      </div>
     </>
   )
 }
