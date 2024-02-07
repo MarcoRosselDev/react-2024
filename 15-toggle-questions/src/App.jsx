d08576db5fdcedcf99d5490091225125fb1391c0
Marco Rossel <andresmarcorossel@gmail.com>
2024-02-07 03:50:07 -0300

    React project reminder 15 | 3

--- 15-toggle-questions/src/App.jsx
+++ 15-toggle-questions/src/App.jsx
@@ -1,39 +1,44 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
+import data from './data'
 
 function App() {
+  const [show, setShow] = useState(null)
+  const [btn, setBtn] = useState(false)
 
-  const [mostras, setMostrar] = useState(false)
-
-  function hidden(elem) {
-    console.log(elem.target.id);
-    setMostrar(anterior => !anterior)
+  function mostrar(id) {
+    id === show? setShow(null) : setShow(id)
+  }
+  // disable, enable
+  function habilitar() {
+    setBtn(prev => !prev)
   }
 
   return (
     <>
       <p>preguntas frecuentes</p>
-
-      <div className='contenedor-preguntas'>
-        <div className='pregunta'>
-          <p onClick={hidden} id='tarea 1'>+ titulo</p>
-          <p>{mostras && 'contenido'}</p>
-        </div>
-        <div className='pregunta'>
-          <p onClick={hidden}>+ titulo</p>
-          <p>{mostras && 'contenido'}</p>
-        </div>
-        <div className='pregunta'>
-          <p onClick={hidden}>+ titulo</p>
-          <p>{mostras && 'contenido'}</p>
-        </div>
-        <div className='pregunta'>
-          <p onClick={hidden}>+ titulo</p>
-          <p>{mostras && 'contenido'}</p>
-        </div>
+      <button onClick={habilitar}>{btn? 'deshabilitar': 'habilitar'}</button>
+      <div className='contenedor'>
+        {
+          data.map(item => (
+            <div className='item' key={item.id}>
+              <p>{item.question}</p>
+              <p>{show === item.id? item.answer : null}</p>
+              <p onClick={() => mostrar(item.id)}>
+                {show === item.id? '-': '+'}
+              </p>
+            </div>
+          ))
+        }
       </div>
     </>
   )
 }
 
 export default App
+
+
+/* function hidden(elem) {
+  console.log(elem.target.id);
+  setMostrar(anterior => !anterior)
+} */
