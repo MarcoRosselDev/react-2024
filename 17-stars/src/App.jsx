0a3801bd3bd56618b175fd5534a33e1b72e82f81
Marco Rossel <andresmarcorossel@gmail.com>
2024-02-15 04:55:35 -0300

    React project reminder 17 | 7

--- 17-stars/src/App.jsx
+++ 17-stars/src/App.jsx
@@ -1,15 +1,42 @@
 import './App.css';
 import start from './star.svg';
 import start_yell from './star-yell.svg';
+import { useState } from 'react';
 
 export default function App(params) {
-  console.log(params);
+  //console.log(params);
+  function createArray(num) {
+    let newArray = []
+    for (let i = 1; i < num + 1; i++) {
+      newArray.push(i)
+    }
+    return newArray
+  }
+  const [starClick, setStartClick] = useState(createArray(5))
+  const [starOver, setStartOver] = useState(createArray(5))
 
+  function changeStar(target, item) {
+    console.log(target);
+    console.log(item);
+  }
+
+  const starsImgs = starOver.map(item => {
+    return (
+      <img 
+      src={start} 
+      key={item} 
+      alt="star valoration"
+      onMouseOver={() => changeStar(item)}
+      />
+    )
+  })
+
+  //console.log(starClick);
   return (
     <div>
-      <p>main</p>
-      <img src={start} alt="empty star" />
-      <img src={start_yell} alt="yellow star" />
+      {starsImgs}
+      {/* <img src={start} alt="empty star" />
+      <img src={start_yell} alt="yellow star" /> */}
     </div>
   )
 }
