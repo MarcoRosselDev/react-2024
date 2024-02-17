d90f8e6a747af5540ab49a3ab5bb4d20ca4c926e
Marco Rossel <andresmarcorossel@gmail.com>
2024-02-17 07:25:24 -0300

    React project reminder 18 | 2

--- 18-img-slider/src/App.jsx
+++ 18-img-slider/src/App.jsx
@@ -1,20 +1,54 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { createClient } from 'pexels';
+
+
+
 
 function App() {
-
-  async function f() {
+  
+  /* async function f() {
     const data = await fetch('https://api.pexels.com/v1/photos', {
       method: 'GET',
       mode: 'no-cors',
+      key: 'mvs1q3C1jBMczdloJxg2DdPO1OkgjpQJQU5wGVZp6HvxaMHw1mOn6KeS'
     })
-
+    
     data.then(info => info.json())
     .then(data => console.log(data))
+  } */
+  
+  async function imgs() {
+    const client = await createClient('mvs1q3C1jBMczdloJxg2DdPO1OkgjpQJQU5wGVZp6HvxaMHw1mOn6KeS');
+    /* const data = await fetch('https://api.pexels.com/v1/', {
+      method: 'GET',
+      //key: 'mvs1q3C1jBMczdloJxg2DdPO1OkgjpQJQU5wGVZp6HvxaMHw1mOn6KeS',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'mvs1q3C1jBMczdloJxg2DdPO1OkgjpQJQU5wGVZp6HvxaMHw1mOn6KeS',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      //body: JSON.stringify(data)
+    })
+    return data */
+    const data = await fetch('https://api.pexels.com/v1/popular', {
+      method: 'GET',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': client,
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+
+    return client
   }
 
   useEffect(() => {
-    f()
+    const data = imgs();
+    data.then((i) => console.log(i))
+    //data.then(a => ())
   }, [])
 
   return (
