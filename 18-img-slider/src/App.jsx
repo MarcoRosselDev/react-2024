2f19cceeae9cd5dc464e62e647bba72e12cadf9a
Marco Rossel <andresmarcorossel@gmail.com>
2024-02-18 20:48:16 -0300

    React project reminder 18 | 9

--- 18-img-slider/src/App.jsx
+++ 18-img-slider/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   const [img, setImg] = useState('')
   const [arr, setArray] = useState([])
-  const [count, setCount] = useState(1)
+  const [count, setCount] = useState(10)
 
   async function getPhotos() {
     const data = await fetch(`https://picsum.photos/v2/list?page=2&limit=10`, {
@@ -14,48 +14,51 @@ function App() {
         "Content-Type": "application/json",
       },
     })
-
-    //console.log(typeof(data));
     return data.json()
   }
 
   useEffect(() => {
-    const variable = getPhotos();
-
-    variable.then(a => {
+    const array_de_fotos = getPhotos();
+    array_de_fotos.then(a => {
       let this_arr = []
       a.map(item => {
         this_arr.push(item)
       })
       setArray(this_arr)
     })
-    //console.log(typeof(variable));
-    //variable.map(item => console.log(item) )
+  }, [])
 
-
-    /* variable.then(info => {
-      console.log(info);
-      console.log(typeof(info.url));
-      setImg(info.url)
+  useEffect(() =>{
+    console.log('new count');
+    arr.map(item => {
+      item.id == count? setImg(item['download_url']): null; 
     })
-    .catch(err => console.log(err)) */
+
   }, [count])
 
-  const elems = arr.map(item => {
+  /* const elems = arr.map(item => {
+    // with: 2500
+    // height: 1667
     return (<img src={item['download_url']} className='imgss' alt='random image' key={item.id} />)
-  })
-  console.log(arr);
+  }) */
+  //console.log(arr);
+
 
   function nextImage() {
-    setCount(prev => prev + 1)
+    setCount(prev => prev>= 19? 10: prev + 1)
+  }
+  function prevImage() {
+    setCount(prev => prev<=10? 19: prev - 1)
   }
 
+  console.log(count);
+
   return (
-    <>
-      {elems}
-      {/* <img src={img} alt="random img" /> */}
-      {/* <button onClick={nextImage}>next image</button> */}
-    </>
+    <div className='contenedor'>
+      <button onClick={prevImage}>prev image</button>
+      <img src={img} alt="imagen random" className='imgss' />
+      <button onClick={nextImage}>next image</button>
+    </div>
   )
 }
 
