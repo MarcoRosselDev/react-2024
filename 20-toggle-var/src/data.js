87018f31e2710283871dfecb8753b750f7c516e4
Marco Rossel <andresmarcorossel@gmail.com>
2024-02-24 19:01:22 -0300

    React project reminder 20 | 2

--- 20-toggle-var/src/data.js
+++ 20-toggle-var/src/data.js
@@ -9,8 +9,47 @@ const data = [
               candelaria: {
                 habitantes: 123101,
                 alcalde: 'lushito faundes'
+              },
+              michaihue: {
+                habitantes: 133101,
+                alcalde: 'lushito faundes'
+              },
+              'boca sur': {
+                habitantes: 103111,
+                alcalde: 'lushito faundes'
               }
             }
+          },
+          'talcahuano' : {
+            poblaciones: {
+
+            }
+          },
+          'concepcion | nonguen': {
+            poblaciones: {
+
+            }
+          },
+          'coronel' : {
+            poblaciones: {
+
+            }
+          }
+        },
+        'habitantes total' : 1214040,
+        region: 'octava'
+      },
+      temuco:{
+        comunas: {
+          'padre las casas':{
+            habitantes: 120441
+          }
+        }
+      },
+      santiago: {
+        comunas: {
+          'puente alto' : {
+            habitantes: 1231245
           }
         }
       }
