650bf46f4bad2e8cd198411cff018574a8da4547
Marco Rossel <andresmarcorossel@gmail.com>
2024-02-27 15:56:47 -0300

    React project reminder 20 | 5

--- 20-toggle-var/src/App.jsx
+++ 20-toggle-var/src/App.jsx
@@ -3,32 +3,55 @@ import data from './data.js'
 
 function App() {
 
-  function clickHandler(params) {
+/*   function overHandler(params) {
     console.log(params);
-    params.map()
+    params? params.map((i) => {
+      return (<li>{i.name}</li>)
+    }): console.log('no tiene hijitos');
   }
 
-  function overF(params) {
-
-  }
   const elems = data.map(item => {
 
     const childrens = item.children? item.children.map(a => {
       return a
     }): null
 
-    return (<li 
+    return (<ul 
       key={item.name} 
-      onClick={() => clickHandler(childrens)}
-      >{item.name}</li>)
+      onMouseOver={() => overHandler(childrens)}
+      >{item.name}
+      </ul>)
   })
-
+ */
   return (
     <>
       <div className='header'>
-        <ul>
-          {elems}
-        </ul>
+        <div>
+          <p>lista 1</p>
+          <ul>
+            <li>a</li>
+            <li>b</li>
+            <li>c</li>
+          </ul>
+        </div>
+        <div>
+          <p>lista 2</p>
+          <ul>
+            <li>d</li>
+            <li>e</li>
+            <li>f</li>
+          </ul>
+        </div>
+        <div>
+          <p>lista 3</p>
+          <ul>
+            <li>g</li>
+            <li>h</li>
+            <div>
+              <p>i</p>
+            </div>
+          </ul>
+        </div>
       </div>
       <div>
         <p>some content</p>
