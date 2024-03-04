03d36e2ab3e9c056c10a58f21846b0c7b311d28e
Marco Rossel <andresmarcorossel@gmail.com>
2024-03-03 22:38:25 -0300

    React project reminder 21 | 1

--- 21-john-smilga/src/App.jsx
+++ 21-john-smilga/src/App.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState([])
+
+  const url = 'https://api.github.com/users'
+  
+  async function github_users() {
+    const data = await fetch(url, {
+      method: "GET"
+    })
+    return data.json()
+  }
+  useEffect(() => {
+    const fetch_data = github_users()
+    fetch_data.then(data => setData(data))
+  }, [])
+
   return (
     <>
-      <p>Hi...</p>
+      <div>
+        <ul>
+          {data.map(item => {
+            return (<li key={item.id}>
+            <img src={item['avatar_url']} alt="user image" />
+              {item.login}
+            </li>)
+          })}
+        </ul>
+      </div>
     </>
   )
 }
