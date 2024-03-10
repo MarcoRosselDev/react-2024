334e7b38f9eaab8422285f105f2f5758ab368953
Marco Rossel <andresmarcorossel@gmail.com>
2024-03-09 22:51:58 -0300

    React project reminder 24 | 2

--- 24-comentarios-usuarios/src/App.jsx
+++ 24-comentarios-usuarios/src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
-import {FaBeer} from 'react-icons/fa'
+import { GoChevronLeft } from "react-icons/go";
+import { GoChevronRight } from "react-icons/go";
 import reviews from './data' 
+import { useState } from 'react';
 
 function App() {
 
+  const [user, setUser] = useState(1)
+
+  function nextUser() {
+    user === 4? setUser(1) : setUser(prev => prev + 1)
+  }
+
+  function prevUser() {
+    user === 1? setUser(4) : setUser(prev => prev - 1)
+  }
+
   const elems = reviews.map(item => {
     return (
-      <div key={item.id}>
+      <div className='div-user' key={item.id}>
         <img src={item.image} alt="user image" />
         <h3>{item.name}</h3>
         <p>{item.job}</p>
@@ -15,11 +27,16 @@ function App() {
     )
   })
 
+  console.log(elems);
+  console.log(user);
+
   return (
     <>
-      <p>Hi</p>
-      <h3>Lets go for a <FaBeer /> </h3>
-      {elems}
+      <div className='contenedor'>
+        <button className='btn-arrow' onClick={prevUser}>{<GoChevronLeft />}</button>
+        {elems[user - 1]}
+        <button className='btn-arrow' onClick={nextUser}>{<GoChevronRight/>}</button>
+      </div>
     </>
   )
 }
