c344ae84e7bd8872c39d1036ade6a06f2d4373f8
Marco Rossel <andresmarcorossel@gmail.com>
2024-03-10 07:29:48 -0300

    React project reminder 25 | 2

--- 25-acordion/src/App.jsx
+++ 25-acordion/src/App.jsx
@@ -1,17 +1,62 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import questions from './data'
 
 function App() {
 
-  const elems = questions.map(item =>{
+  const [toggle, setToggle] = useState([])
+
+  /* function deleteOne(id) {
+    func(prev => {
+      const redu = prev.reduce((final, item) => {
+        if (item.id == id) {
+          console.log('hi');
+        } else {
+          final.push(item)
+        }
+        return final
+      }, [])
+      console.log(redu);
+      return redu
+    })
+  } */
+  function cargar_array() {
+    const redu = questions.reduce((final, item) =>{
+      final.push({
+        id: item.id,
+        title: item.title,
+        key: item.id,
+        info: item.info,
+        mostrar: false
+      })
+      return final
+    }, [])
+    setToggle(redu)
+  }
+
+  useEffect(() =>{
+    cargar_array()
+  }, [])
+  
+  function mostrar(id) {
+    console.log(id);
+    const a = Number(id) -1
+    setToggle(prev => {
+      return [...prev, {
+        prev[a].id == id? console.log('hi'): console.log(true)
+      }]
+    })
+  }
+  console.log(toggle, 'mostrar');
+
+  const elems = toggle.map(item =>{
     return (
       <div className='item' key={item.id}>
         <div className='div-title'>
           <h2>{item.title}</h2>
-          <button className='btn'>-</button>
+          <button className='btn' onClick={() => mostrar(item.id)} >{item.mostrar? '-' : '+'}</button>
         </div>
-        <p>{item.info}
-        </p>
+        <p>{item.mostrar? item.info : ''}</p>
       </div>
     )
   })
